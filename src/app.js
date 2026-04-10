95416a249238f9c9132def3d9d0311ccfc3df62e
Ayush Sharma <sahilsharma07a@gmail.com>
2026-04-11 00:19:03 +0530

    just adding cors method

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const app=express();
 // middlewares for parsing the data from the frontend
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(cors());
 
 // router entry point 
 import userRouter from "./routes/router.routes.js"
